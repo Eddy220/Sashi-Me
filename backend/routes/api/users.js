0e985b2abe32a24128b9540fdeaab74f1147af2f
Eddy <edkim220@gmail.com>
2021-05-20 17:37:13 -0400

    user signup API route complete

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
 
+// Sign up API route will call on signup static method in User model
+// if user is successfully created, then call setTokenCookie, return JSON
+// res with user info
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
 
 module.exports = router;
