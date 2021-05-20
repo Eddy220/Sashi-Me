3230063a9fbb9c02311faf553019967dd3d3e7cb
Eddy <edkim220@gmail.com>
2021-05-20 18:11:51 -0400

    signup validators complete

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,13 +2,36 @@ const express = require('express')
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
 
 // Sign up API route will call on signup static method in User model
 // if user is successfully created, then call setTokenCookie, return JSON
 // res with user info
 router.post(
   '/',
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
