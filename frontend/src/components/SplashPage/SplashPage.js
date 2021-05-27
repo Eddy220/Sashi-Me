bd58bc59350e479ed528ee2cace333cef5275253
Eddy <edkim220@gmail.com>
2021-05-27 02:23:42 -0400

    added logo and favicon

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -3,13 +3,18 @@ import './SplashPage.css';
 
 function SplashPage() {
   return (
-    <div className='splashPageImage'>
-      {/* <div>
-        <img src='Sashi-Me.png' className='picture'></img>
-      </div> */}
-      <div className='searchBar'>
-        <input className='searchInput' type='txt' placeholder='Search for a restaurant...'></input>
-        <button className='searchBtn' type='submit'>Search!</button>
+    <div>
+      <div className='logoContainer'>
+       <img className='logo' src='../../Sashi-Me.png'></img>
+      </div>
+      <div className='splashPageImage'>
+        {/* <div>
+          <img src='Sashi-Me.png' className='picture'></img>
+        </div> */}
+        <div className='searchBar'>
+          <input className='searchInput' type='txt' placeholder='Search for a restaurant...'></input>
+          <button className='searchBtn' type='submit'>Search!</button>
+        </div>
       </div>
     </div>
   )
